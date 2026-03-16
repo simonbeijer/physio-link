650ebcacb2ba564d1b5fe31362dfff0270bf387f
Simon Beijer <beijer.simon@gmail.com>
2026-03-16 17:28:59 +0100

    Add apiFetch helper for API URL

---
+++ vue/src/lib/api.js
@@ -0,0 +1,15 @@
+const BASE_URL = import.meta.env.VITE_API_URL
+
+export async function apiFetch(endpoint, options = {}) {
+  const token = localStorage.getItem('token')
+  
+  return fetch(`${BASE_URL}${endpoint}`, {
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${token}`,
+      ...options.headers
+    }
+  })
+}
--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -3,4 +3,29 @@
     <h1 class="text-2xl font-bold">Dashboard</h1>
     <p>Du är inloggad!</p>
   </div>
+  <div>
+    <Button @click="getSchemas">Hämta scheman</Button>
+  </div>
 </template>
+<script setup>
+import Button from '@/components/ui/button/Button.vue';
+import { apiFetch } from '@/lib/api.js'
+
+
+async function getSchemas() {
+  try {
+    const token = localStorage.getItem('token')
+    const response = await apiFetch('/api/schemas')
+
+    const data = response.json()
+
+    if (!response.ok) {
+      return
+    }
+    console.log(data)
+
+  } catch (error) {
+    error.value = 'Något gick fel'
+  }
+}
+</script>
