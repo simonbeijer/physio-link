4caca4643c916238e1b0f926c94ef264af6b7f35
Simon Beijer <beijer.simon@gmail.com>
2026-04-27 16:13:14 +0200

    Change to postgreSQL database

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => env('SEED_USER_NAME', 'Test User'),
+            'email' => env('SEED_USER_EMAIL', 'test@example.com'),
+            'password' => bcrypt(env('SEED_USER_PASSWORD', 'password')),
         ]);
     }
 }
